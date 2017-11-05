7a7be15eb1a6dfdc85efab38ea6306641b6c264a
Hex <mdpung@gmail.com>
2017-11-05 00:58:22 -0500

    Changed Delay
    Changed Delay to 1ms for better reponse time.
    
    Removed unused constants from previous plans.

--- SimpleOutputFromPoses/SimpleOutputFromPoses.ino
+++ SimpleOutputFromPoses/SimpleOutputFromPoses.ino
@@ -1,20 +1,18 @@
 #include <MyoController.h>
 
-#define FIST_PIN 6
-#define WAVEIN_PIN 5
 #define WAVEOUT_PIN 7
-#define FINGERSSPREAD_PIN 4
+#define FINGERSSPREAD_PIN 6
+#define WAVEIN_PIN 5
+#define FIST_PIN 4
 #define DOUBLETAP_PIN 12
 #define TRANSISTOR_ACTIVE HIGH
 #define TRANSISTOR_INACTIVE LOW
 
 MyoController myo = MyoController();
 
-
 void setup() {
-
-  pinMode(FIST_PIN, OUTPUT);
   pinMode(WAVEIN_PIN, OUTPUT);
+  pinMode(FIST_PIN, OUTPUT);
   pinMode(WAVEOUT_PIN, OUTPUT);
   pinMode(FINGERSSPREAD_PIN, OUTPUT);
   pinMode(DOUBLETAP_PIN, OUTPUT);
@@ -22,9 +20,7 @@ void setup() {
   myo.initMyo();
 }
 
-void loop()
-{
-   //Serial.println("HI");
+void loop() {
    myo.updatePose();
    switch ( myo.getCurrentPose() ) {
     case rest:
@@ -50,5 +46,5 @@ void loop()
       digitalWrite(DOUBLETAP_PIN,LOW);
       break;
    } 
-   delay(10);
+   delay(1);
 }
